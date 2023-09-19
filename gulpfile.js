e8d698dacf326757f91c0f839e5829daed5d47c9
misha <mishapolitik29@gmail.com>
2023-09-19 17:46:10 +0300

    init

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+import gulp from "gulp";
+import gulpSass from "gulp-sass";
+import browserSync from "browser-sync";
+import * as dartSass from "sass";
+
+const sass = gulpSass(dartSass);
+const browserSyncObj = browserSync.create();
+
+gulp.task("sass", function (done) {
+  gulp
+    .src("./src/styles/**/*.scss")
+    .pipe(sass.sync().on("error", sass.logError))
+    .pipe(gulp.dest("./src/styles/css"))
+    .pipe(browserSyncObj.stream());
+
+  done();
+});
+gulp.task("serve", function (done) {
+  browserSyncObj.init({
+    server: "./",
+    port: 3000,
+  });
+
+  gulp.watch("./src/styles/**/*.scss", gulp.series("sass"));
+  gulp.watch("*.html").on("change", () => {
+    browserSync.reload();
+    done();
+  });
+  done();
+});
+
+gulp.task("default", gulp.series("sass", "serve"));
